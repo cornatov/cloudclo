84e83c682be94363144edff9a939801554f1625b
Cornato Vella <cornatov@buffalo.edu>
2018-09-11 12:13:13 +0200

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :campaigns
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'campaigns#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

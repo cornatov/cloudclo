4c528827c563a0f6075c2acff4a93fe77d4a9899
Cornato Vella <cornatov@buffalo.edu>
2018-09-11 22:04:45 +0200

    Added emailing to both parties

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,10 +1,19 @@
 class CampaignMailer < ApplicationMailer
-	default from: "noreply@world.com"
+	default from: 'noreply@non.com'
 
 	def send_campaign(campaign)
 		@campaign = campaign
-		mail(to: @campaign.to_email)
+		mail(to: "cornatov@buffalo.edu", subject: "Add email", body: '#{campaign.to_email}' )
 	end
+
+	def send_campaign(campaign)
+		@campaign = campaign
+		mail(to: @campaign.to_email, subject: "Thank you for joining", body: "You have been added to our list!")
+	end
+
+
+
+
 
 end
 
